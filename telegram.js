ce666cee35d5db6f8bf938bf46e6a23733ebd51b
Maxim Kisselyov <mkisselyov@me.com>
2022-10-28 10:01:55 +0600

    Ready Code

--- telegram.js
+++
@@ -1,7 +0,0 @@
-//5356945209:AAGH85xdo7g623pB_3Cxe4MEc_DqpNyXrZ8
-
-
-curl -X POST \
-     -H 'Content-Type: application/json' \
-     -d '{"chat_id": "529468682", "text": "This is a test from curl", "disable_notification": true}' \
-     https://api.telegram.org/bot5356945209:AAGH85xdo7g623pB_3Cxe4MEc_DqpNyXrZ8/sendMessage
