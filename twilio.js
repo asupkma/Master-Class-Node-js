073e5d005e28b3f561ca99da28d8ad9f8e58faf6
Maxim Kisselyov <mkisselyov@me.com>
2022-10-28 09:55:20 +0600

    Ready Code

--- twilio.js
+++ twilio.js
@@ -19,7 +19,7 @@ client.messages
       .then(message => console.log(message.sid))
       .done();
 
-
+//sdff
 //     'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
 //   'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
 //     'fromPhone' : '+15005550006'
